9bf01ace50f3ac00456203ec8d3ca036de478284
sadewo645 <jamrosadewo@gmail.com>
2025-11-05 15:28:02 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Ganti 'palm-oil-dashboard' sesuai nama repositori kamu
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/',
+  base: '/palm-oil-dashboard/', // 👈 penting agar GitHub Pages bisa memuat asset-nya
 })
